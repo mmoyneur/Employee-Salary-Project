c3de23b33e091428c4a9aa926e696d4a7546ad03
mmoyneur <mmoyneur808@gmail.com>
2022-03-30 02:48:07 -0700

    Create Form1.Designer.cs

---
+++ Moyneur_Course_Project_Part2/Moyneur_Course_Project_Part2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Moyneur_Course_Project_Part2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.PrintPaychecksButton = new System.Windows.Forms.Button();
+            this.EmployeesListBox = new System.Windows.Forms.ListBox();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(35, 48);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(124, 38);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveButton.Location = new System.Drawing.Point(150, 48);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(124, 38);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayButton.Location = new System.Drawing.Point(269, 48);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(124, 38);
+            this.DisplayButton.TabIndex = 2;
+            this.DisplayButton.Text = "display";
+            this.DisplayButton.UseVisualStyleBackColor = true;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // PrintPaychecksButton
+            // 
+            this.PrintPaychecksButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrintPaychecksButton.Location = new System.Drawing.Point(389, 48);
+            this.PrintPaychecksButton.Name = "PrintPaychecksButton";
+            this.PrintPaychecksButton.Size = new System.Drawing.Size(180, 38);
+            this.PrintPaychecksButton.TabIndex = 3;
+            this.PrintPaychecksButton.Text = "Print Paychecks";
+            this.PrintPaychecksButton.UseVisualStyleBackColor = true;
+            this.PrintPaychecksButton.Click += new System.EventHandler(this.PrintPaychecksButton_Click);
+            // 
+            // EmployeesListBox
+            // 
+            this.EmployeesListBox.FormattingEnabled = true;
+            this.EmployeesListBox.Location = new System.Drawing.Point(38, 129);
+            this.EmployeesListBox.Name = "EmployeesListBox";
+            this.EmployeesListBox.Size = new System.Drawing.Size(686, 316);
+            this.EmployeesListBox.TabIndex = 4;
+            this.EmployeesListBox.DoubleClick += new System.EventHandler(this.EmployeesListBox_DoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 518);
+            this.Controls.Add(this.EmployeesListBox);
+            this.Controls.Add(this.PrintPaychecksButton);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Name = "MainForm";
+            this.Text = "Payroll System";
+           // ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.Button PrintPaychecksButton;
+        private System.Windows.Forms.ListBox EmployeesListBox;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+    }
+}
+
