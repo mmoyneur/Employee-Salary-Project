ef825dfa096246e15a53ff63d0d531cb7017d83c
mmoyneur <mmoyneur808@gmail.com>
2022-03-30 02:48:03 -0700

    Create Employee.cs

---
+++ Moyneur_Course_Project_Part2/Moyneur_Course_Project_Part2/Employee.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moyneur_Course_Project_Part2
+{
+    internal class Employee
+    {
+        protected string firstName;
+        protected string lastName;
+        protected string ssn;
+        protected DateTime hireDate;
+        protected Benefits benefits;
+
+        //default constructor
+        public Employee()
+        {
+            firstName = "Tom";
+            lastName = "Brady";
+            ssn = "unknown";
+            hireDate = DateTime.MinValue;
+        }
+        //parameterized constructor
+        
+        public Employee(string fname, string lname, string ssn, DateTime hdate, Benefits benefits)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;                          
+            this.SSN = ssn;
+            this.HireDate = hdate;
+            this.BenefitsPackage = benefits;
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName+", SSN: " + ssn + ", HireDate: " + hireDate.ToShortDateString();
+        }
+        public virtual double CalculatePay()
+        {
+            return 0.0;
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length > 0)
+                    firstName = value;
+                else
+                    firstName = "unknown";
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length > 0)
+                    lastName = value;
+                else
+                    lastName = "unknown";
+            }
+        }
+        public string SSN
+        {
+            get { return ssn; }
+            set
+            {
+                if(value.Length == 9 || value.Length == 11)
+                    ssn = value;
+                else
+                    ssn = "unknown";
+            }
+        }
+        public DateTime HireDate
+        {
+            get { return hireDate; }
+            set
+            {
+                if(value.Year >=1950 && value.Year < DateTime.Now.Year + 1)
+                    hireDate = value;
+                else
+                    hireDate = DateTime.MinValue;
+            }
+        }
+        public Benefits BenefitsPackage
+        {
+            get { return benefits; }
+            set { this.benefits = value; }
+        }
+    }
+}
