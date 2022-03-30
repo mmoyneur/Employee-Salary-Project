0fca83be0ecc2b9ed9374d950b702281b529d721
mmoyneur <mmoyneur808@gmail.com>
2022-03-30 02:47:46 -0700

    Create Benefits.cs

---
+++ Moyneur_Course_Project_Part2/Moyneur_Course_Project_Part2/Benefits.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moyneur_Course_Project_Part2
+{
+    internal class Benefits
+    {
+        //attributes
+        private string healthInsurance;
+        private double lifeInsurance;
+        private int vacation;
+        //constructors
+        public Benefits()
+        {
+            healthInsurance = "unknown";
+            lifeInsurance = 0.0;
+            vacation = 0;
+        }
+        public Benefits(string healthInsurance, double lifeInsurance, int vacation)
+        {
+            this.HealthInsurance = healthInsurance;
+            this.LifeInsurance = lifeInsurance;
+            this.Vacation = vacation;
+        }
+        //method
+        public override string ToString()
+        {
+            return "Health Insurance: " + healthInsurance + ", Life Insurance: " + lifeInsurance + ", Vacation days: " + vacation;
+        }
+
+        //properties
+        public string HealthInsurance
+        {
+            get { return healthInsurance; }
+            set
+            {
+                if (value.Length > 0)
+                    healthInsurance = value;
+                else
+                    healthInsurance = "unknown";
+            }
+        }
+        public double LifeInsurance
+        {
+            get { return lifeInsurance;}
+            set
+            {
+                if (value > 0.0 && value <= 10000000000.0)
+                    lifeInsurance = value;
+                else
+                    lifeInsurance = 0.0;
+            }
+        }
+        public int Vacation
+        {
+            get { return vacation; }
+            set
+            {
+                if (value > 0.0 && value <= 40)
+                    vacation = value;
+                else
+                    vacation = 0;
+            }
+        }
+    }
+}
